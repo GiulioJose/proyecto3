5b5b2ac116d6e7d3ba79c7e5b83fded564f6a5f5
giuliojose <giuilio.asesor23@gmail.com>
2025-03-24 12:18:56 +0100

    v1.1 primer avanze del header

---
+++ src/js/header.js
@@ -0,0 +1,37 @@
+const headerTemplate = () => {
+  return `
+  <h1>I</h1>
+  <input type="text" placeholder="Search" id="searchinput"/>
+  <button id="searchbtn">
+    <img src="/icons/search.svg" alt="Search icon"/>
+  </button>
+  <button id="darkmodebtn">
+    <img src="/icons/dark.svg" alt="Dark mode icon" id="darkmodeicon/">
+  </button>
+  <img src="/images/profile.jpg" alt="Profile image" class="profileimg"/>
+`;
+};
+
+const themeSwitch = () => {
+  document.body.classList.toggle("dark");
+};
+
+const listeners = () => {
+  const darkmodebtn = document.querySelector("#darkmodebtn");
+  darkmodebtn.addEventListener("click", () => {
+    themeSwitch();
+    const theme = document.body.classList.contains("dark");
+    if (theme) {
+      document.querySelector("#darkmodeicon").src = "/icons/light.svg";
+    } else {
+      document.querySelector("#darkmodeicon").src = "/icons/dark.svg";
+    }
+  });
+};
+
+const printHeaderTemplate = () => {
+  document.querySelector("header").innerHTML = headerTemplate();
+  listeners();
+};
+
+printHeaderTemplate();
--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+//  Styles
 import './styles/style.scss';
+
+//  Components
+import './js/header.js';
+
 
 document.querySelector('#app').innerHTML = ` <div> </div>`
